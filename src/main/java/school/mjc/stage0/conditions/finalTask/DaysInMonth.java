bdfb3e56c32464beb7ca3a79342e57546f41f53c
Saidahmad95 <murtazoyev00@gmail.com>
2022-11-03 00:53:01 +0500

    first commit

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,21 @@
 package school.mjc.stage0.conditions.finalTask;
 
+import java.time.DateTimeException;
+import java.time.Month;
+
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        try {
+            Month of = Month.of(month);
+            if (((((year % 4) == 0) && ((year % 100) != 0)) || year % 400 == 0)) {
+                System.out.println(of.length(true));
+            } else {
+                System.out.println(of.length(false));
+            }
+        } catch (DateTimeException e) {
+            System.out.println("invalid date");
+        }
+
 
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        try {
+            int i = dividend / divider;
+            System.out.println(i * divider == dividend ? "can be divided completely" : "cannot be divided completely");
+        }catch (ArithmeticException e){
+            System.out.println( "division by zero");
+        }
     }
 }
